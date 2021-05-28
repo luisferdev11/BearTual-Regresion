b3e8de6130d022577cbecd6693e5cd7e3a9433ce
Luis Fernando Rodríguez Domínguez <lrodriguezd1900@alumno.ipn.mx>
2021-05-28 01:21:27 -0500

    Regresiones de paises completas en local (falta implementar a web)

--- Regresiones.py
+++ Regresiones.py
@@ -3,19 +3,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
-#from Usuario import *
+# from Usuario import *
 
-df_main = pd.read_csv("datadepression.csv", header=0)
-df_main.columns = ["pais", "year", "depr"]
+columnas = ("pais", "year", "depr")
 
-print(df_main)
 
 def regresion(df_main, pais):
     df = df_main.query(f"pais=='{pais}'")
-    X = np.array(df.year.values)
+    X = np.array(df.year)
     y = np.array(df.depr)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_state=42)
     X_train = X_train.reshape([X_train.shape[0], 1])
     X_test = X_test.reshape([X_test.shape[0], 1])
 
@@ -24,80 +22,46 @@ def regresion(df_main, pais):
 
     Y_pred = lr.predict(X_test)
 
-    plt.scatter(X, y)
-    plt.plot(X_test, Y_pred, color="red", linewidth=3)
-    plt.show()
-    print(df)
+    graf = str(input("Desea graficar?: "))
+    if graf in ("S, s"):
+        graficar(X,y, X_test, Y_pred, df, lr, pais)
+    else:
+        predecir(df, lr)
+
+def graficar(X, y, X_test, Y_pred, df, lr, pais):
+    plt.title(f'Porcentaje de personas con depresión en {pais}')
+    plt.ylabel('Porcentaje')
+    plt.xlabel('Año')
+    plt.scatter(X, y, c="#2EC5CE")
+    plt.plot(X_test, Y_pred, c="#8C30F5", linewidth=3)
+
+    pred = str(input("Desea predecir?: "))
+    if pred in ("S, s"):
+        predecir(df,lr)
+        plt.scatter(entrada, prediccion, c="#FE9A22")
+        plt.show()
+    else:
+        plt.show()
+
+def predecir(df,lr):
+    try:
+        global entrada, prediccion
+        entrada = np.array([0,])
+        prediccion = None
+        while (entrada[0] < 2021) or (entrada[0] > 2030):
+            entrada[0] = input("Escriba el valor a predecir: ")
+            entrada = entrada.reshape([entrada.shape[0], 1])
+            prediccion = lr.predict(entrada)
+        print(df)
+        print(prediccion)
+    except:
+        print("Valor inválido")
+
+
+df_main = pd.read_csv("datadepression.csv", header=0)
+df_main.columns = columnas
+print(df_main)
 
 regresion(df_main, "CAN")
 regresion(df_main, "USA")
 regresion(df_main, "MEX")
-
-"""
-#CANADA
-df_CAN = df_main.query("pais=='CAN'")
-X = np.array(df_CAN.year.values)
-Y_CAN = np.array(df_CAN.depr)
-
-# Y_CAN_pred = 20.40 - (0.008171*X)
-X_train, X_test, y_train, y_test = train_test_split(X, Y_CAN, test_size=0.4, random_state=42)
-
-X_train = X_train.reshape([X_train.shape[0],1])
-X_test = X_test.reshape([X_test.shape[0],1])
-
-#Entrenar el modelo
-lr_CAN = linear_model.LinearRegression()
-lr_CAN.fit(X_train, y_train)
-
-#Prediccion
-Y_CAN_pred = lr_CAN.predict(X_test)
-
-plt.scatter(X, Y_CAN)
-plt.plot(X_test, Y_CAN_pred, color="red", linewidth=3)
-plt.show()
-
-
-
-#USA
-df_USA = df_main.query("pais=='USA'")
-Y_USA = np.array(df_USA.depr)
-
-X_train, X_test, y_train, y_test = train_test_split(X, Y_USA, test_size=0.4, random_state=42)
-
-X_train = X_train.reshape([X_train.shape[0],1])
-X_test = X_test.reshape([X_test.shape[0],1])
-
-#Y_USA_pred = -7.54 + (0.00613*X)
-lr_USA = linear_model.LinearRegression()
-lr_USA.fit(X_train, y_train)
-
-Y_USA_pred = lr_USA.predict(X_test)
-plt.scatter(X, Y_USA)
-plt.plot(X_test, Y_USA_pred, color="red", linewidth=3)
-plt.show()
-print(df_USA)
-
-
-
-#MEXICO
-df_MEX = df_main.query("pais=='MEX'")
-Y_MEX = np.array(df_MEX.depr)
-
-X_train, X_test, y_train, y_test = train_test_split(X, Y_MEX, test_size=0.4, random_state=42)
-
-X_train = X_train.reshape([X_train.shape[0],1])
-X_test = X_test.reshape([X_test.shape[0],1])
-
-#Y_MEX_pred = -5.30 + (0.004014*X)
-lr_MEX = linear_model.LinearRegression()
-lr_MEX.fit(X_train, y_train)
-
-Y_MEX_pred = lr_MEX.predict(X_test)
-
-plt.scatter(X, Y_MEX)
-plt.plot(X_test, Y_MEX_pred, color="red", linewidth=3)
-plt.show()
-print(df_MEX)
-"""
-
-
